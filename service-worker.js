356f3a810f8dec6017c50191f1075794e24bf71e
LoneRangeGamer <87843570+LoneRangeGamer@users.noreply.github.com>
2025-06-13 10:55:32 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'smart-dryer-v1';
 const urlsToCache = [
-  '/index.html',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png',
+  '/LoneRangeGamer/index.html',
+  '/LoneRangeGamer/icons/icon-192x192.png',
+  '/LoneRangeGamer/icons/icon-512x512.png',
   'https://cdn.tailwindcss.com',
   'https://cdn.jsdelivr.net/npm/chart.js',
   'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&display=swap'
