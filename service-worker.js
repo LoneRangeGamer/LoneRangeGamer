56deb6a418af868512ed424c6dcca0a903320b50
LoneRangeGamer <87843570+LoneRangeGamer@users.noreply.github.com>
2025-06-13 11:33:21 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,25 +3,21 @@ const urlsToCache = [
   '/LoneRangeGamer/index.html',
   '/LoneRangeGamer/icons/icon-192x192.png',
   '/LoneRangeGamer/icons/icon-512x512.png',
-  'https://cdn.tailwindcss.com',
   'https://cdn.jsdelivr.net/npm/chart.js',
   'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&display=swap'
 ];
-
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
-
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
   );
 });
-
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
